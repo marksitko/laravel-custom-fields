0e1b8c2ad5981a63631c63cadfaf717c59008b0d
Mark Sitko <mark@sitko-designing.de>
2021-01-04 20:46:57 +0100

    Add updateOrCreateFields method to HasCustomFieldResponses Trait

--- src/Traits/HasCustomFieldResponses.php
+++ src/Traits/HasCustomFieldResponses.php
@@ -27,6 +27,34 @@ trait HasCustomFieldResponses
         }
     }
 
+    public function updateOrCreateFields($fields)
+    {
+        foreach ($fields as $key => $value) {
+            $customField = config('custom-fields.models.custom_field')::find((int) $key);
+
+            if (! $customField) {
+                continue;
+            }
+
+            if ($customFieldResponse = config('custom-fields.models.custom_field_response')::where([
+                'field_id' => $customField->id,
+                'model_id' => $this->id,
+                'model_type' => get_class($this),
+            ])->first()) {
+                $customFieldResponse->update([
+                    'value' => $value,
+                ]);
+            } else {
+                config('custom-fields.models.custom_field_response')::create([
+                    'value' => $value,
+                    'field_id' => $customField->id,
+                    'model_id' => $this->id,
+                    'model_type' => get_class($this),
+                ]);
+            }
+        }
+    }
+
     public function scopeWhereField($query, $field, $value)
     {
         $query->whereHas('customFieldResponses', function ($subQuery) use ($field, $value) {
